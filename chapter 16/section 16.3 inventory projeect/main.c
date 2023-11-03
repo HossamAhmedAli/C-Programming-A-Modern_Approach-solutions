81f1ae2eda50c7ecd14798a97bb33ff63ba30afd
HossamAhmedAli <56847513+HossamAhmedAli@users.noreply.github.com>
2023-11-03 19:59:58 +0200

    Add files via upload

---
+++ chapter 16/section 16.3 inventory projeect/main.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "declarations.h"
+
+int main(void)
+{
+	char choice_input;
+	while(choice_input != 't')
+	{
+
+		printf("i: Insert ... s: search ... p: print ... u: update ...  t: terminate program \n");
+		fflush(stdout);
+		scanf("%s",&choice_input);
+
+		switch(choice_input)
+		{
+		case INSERT:
+			insert();
+			break;
+
+		case SEARCH:
+			search();
+			break;
+
+		case PRINT:
+			display_inventory();
+			break;
+		case UPDATE:
+			update();
+			break;
+        case 't':
+            break;
+
+		default:
+			printf("Invalid Input .. plz try again\n");
+			break;
+
+		}// end switch
+
+	}// end while(1)
+}
